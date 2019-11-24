a8f72c08283d689a693d045e1ce01b7104be11fc
Lee Faria <>
2019-11-24 14:44:42 -0500

    Initial Commit

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -13,25 +13,27 @@ contract SimpleBank {
     //
     
     /* Fill in the keyword. Hint: We want to protect our users balance from other contracts*/
-    mapping (address => uint) balances;
+    mapping (address => uint) private balances;
     
     /* Fill in the keyword. We want to create a getter function and allow contracts to be able to see if a user is enrolled.  */
-    mapping (address => bool) enrolled;
+    mapping (address => bool) public enrolled;
 
     /* Let's make sure everyone knows who owns the bank. Use the appropriate keyword for this*/
-    address owner;
+    address public owner = msg.sender ;
     
     //
     // Events - publicize actions to external listeners
     //
     
     /* Add an argument for this event, an accountAddress */
-    event LogEnrolled();
+    event LogEnrolled(address _accountAddress);
 
     /* Add 2 arguments for this event, an accountAddress and an amount */
-    event LogDepositMade();
+    event LogDepositMade(address _accountAddress, uint amount);
 
     /* Create an event called LogWithdrawal */
+
+    event LogWithdrawlMade(address _accountAddress, uint amount)
     /* Add 3 arguments for this event, an accountAddress, withdrawAmount and a newBalance */
 
 
@@ -40,9 +42,10 @@ contract SimpleBank {
     //
 
     /* Use the appropriate global variable to get the sender of the transaction */
-    constructor() public {
-        /* Set the owner to the creator of this contract */
-    }
+    // constructor() public {
+    //     /* Set the owner to the creator of this contract */
+    //     msg.sender = owner;
+    // }
 
     // Fallback function - Called if other functions don't match call or
     // sent ether without data
@@ -57,14 +60,24 @@ contract SimpleBank {
     /// @return The balance of the user
     // A SPECIAL KEYWORD prevents function from editing state variables;
     // allows function to run locally/off blockchain
-    function getBalance() public returns (uint) {
+    function getBalance(address _address) public view returns (uint) {
         /* Get the balance of the sender of this transaction */
+        return balances[_address];
     }
 
     /// @notice Enroll a customer with the bank
     /// @return The users enrolled status
     // Emit the appropriate event
     function enroll() public returns (bool){
+
+         enrolled[msg.sender] = true;
+        emit LogEnrolled(msg.sender);
+        
+        return enrolled[msg.sender];
+        
+      
+
+
     }
 
     /// @notice Deposit ether into bank
@@ -73,9 +86,13 @@ contract SimpleBank {
     // Use the appropriate global variables to get the transaction sender and value
     // Emit the appropriate event    
     // Users should be enrolled before they can make deposits
-    function deposit() public returns (uint) {
+    function deposit() public  payable returns  (uint) {
         /* Add the amount to the user's balance, call the event associated with a deposit,
           then return the balance of the user */
+          balances[msg.sender] += msg.value;
+          emit LogDepositMade(msg.sender , msg.value);
+          return balances[msg.sender];
+
     }
 
     /// @notice Withdraw ether from bank
